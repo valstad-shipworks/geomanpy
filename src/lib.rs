3e32d21a1bf12c19feee19c63d4b21a75a7a3cae
oh-yes-0-fps <foryt.bowan@gmail.com>
2026-04-14 12:59:43 -0500

    added a way for rust python module importers to more easily export type hints of this module

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+#[cfg(feature = "not_build_only")]
 pub mod glam_wrappers;
+#[cfg(feature = "not_build_only")]
 pub mod wreck_wrappers;
 
+#[cfg(feature = "not_build_only")]
 use pyo3::prelude::*;
 
+pub const PYI_CONTENTS: &str = include_str!("../py_src/geomanpy/__init__.pyi");
+pub const PY_MODULE_CONTENTS: &str = include_str!("../py_src/geomanpy/__init__.py");
+
+#[cfg(feature = "not_build_only")]
 #[pymodule(name = "_geomanpy")]
 fn geomanpy(m: &Bound<'_, PyModule>) -> PyResult<()> {
     glam_wrappers::register(m)?;
